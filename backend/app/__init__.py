c2986dc6b8682888e834c67ad31a636c8511b370
MananSoni <manansoni.soni77@gmail.com>
2023-10-11 23:09:42 +0530

    update redis link

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,9 @@
 from celery import Celery
 
+
 def make_celery():
-    return Celery(__name__, broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+    # return Celery(__name__, broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+    return Celery(__name__, broker='redis://red-ckjd28a12bvs739tgvg0:6379', backend='redis://red-ckjd28a12bvs739tgvg0:6379')
+
 
 client = make_celery()
